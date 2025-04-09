037130bd92b844a7125da59f158c8acdace02075
fujis <ppdx999@gmail.com>
2025-04-09 22:46:56 +0900

    implement download command

--- cmd/download.go
+++ cmd/download.go
@@ -1,40 +1,151 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// downloadCmd represents the download command
 var downloadCmd = &cobra.Command{
-	Use:   "download",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "download",
+	Short:   "問題のサンプルケースをダウンロードします",
+	Aliases: []string{"d"},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("download called")
+		runDownload()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(downloadCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func runDownload() {
+	config, err := getConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fail to get config: %v\n", err)
+		os.Exit(1)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// downloadCmd.PersistentFlags().String("foo", "", "A help for foo")
+	samples, err := fetchSamples(config)
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// downloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fail to get samples: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := writeSamples(samples); err != nil {
+		fmt.Fprintf(os.Stderr, "Fail to write samples: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+type config struct {
+	contest string
+	problem string
+}
+
+func getConfig() (c config, err error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	c.problem = filepath.Base(cwd)
+	c.contest = filepath.Base(filepath.Dir(cwd))
+	return
+}
+
+type sample struct {
+	in  string
+	out string
+}
+
+func (s sample) String() string {
+	return fmt.Sprintf("in: %s, out: %s", s.in, s.out)
+}
+
+func fetchSamples(c config) ([]sample, error) {
+	body, err := fetchProblemPage(c)
+	if err != nil {
+		return nil, err
+	}
+	return parseSamples(body)
+}
+
+func fetchProblemPage(c config) (io.Reader, error) {
+	url := fmt.Sprintf("https://atcoder.jp/contests/%s/tasks/%s_%s", c.contest, strings.ReplaceAll(c.contest, "-", "_"), c.problem)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("HTTPリクエスト失敗: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTPエラー: %s", resp.Status)
+	}
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(buf), nil
+}
+
+func parseSamples(r io.Reader) ([]sample, error) {
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("読み込み失敗: %w", err)
+	}
+	html := string(data)
+
+	// 入力例・出力例それぞれに続く <pre> の内容を抽出
+	re := regexp.MustCompile(`<h3>(入力例|出力例)\s*\d+</h3>\s*<pre>([\s\S]*?)</pre>`)
+	matches := re.FindAllStringSubmatch(html, -1)
+
+	if len(matches)%2 != 0 {
+		return nil, fmt.Errorf("入力と出力のペアが揃っていません")
+	}
+
+	var samples []sample
+	for i := 0; i+1 < len(matches); i += 2 {
+		if matches[i][1] != "入力例" || matches[i+1][1] != "出力例" {
+			return nil, fmt.Errorf("入力/出力の順番が想定と異なります")
+		}
+		in := strings.TrimSpace(matches[i][2])
+		out := strings.TrimSpace(matches[i+1][2])
+		samples = append(samples, sample{in, out})
+	}
+	return samples, nil
+}
+
+func writeSamples(samples []sample) error {
+	testDir := "test"
+
+	if err := os.MkdirAll(testDir, 0755); err != nil {
+		return fmt.Errorf("テストディレクトリ作成失敗: %w", err)
+	}
+
+	for i, s := range samples {
+		inPath := filepath.Join(testDir, fmt.Sprintf("%d.in", i))
+		outPath := filepath.Join(testDir, fmt.Sprintf("%d.out", i))
+
+		if err := os.WriteFile(inPath, []byte(s.in), 0644); err != nil {
+			return fmt.Errorf("入力ファイル書き込み失敗: %w", err)
+		}
+		if err := os.WriteFile(outPath, []byte(s.out), 0644); err != nil {
+			return fmt.Errorf("出力ファイル書き込み失敗: %w", err)
+		}
+	}
+
+	return nil
 }
---
+++ cmd/download_test.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseSamples(t *testing.T) {
+	html := `
+	<div class="part">
+	<section>
+	<h3>入力例 1</h3><pre>10
+	</pre>
+	</section>
+	</div>
+
+	<div class="part">
+	<section>
+	<h3>出力例 1</h3><pre>40
+	</pre>
+	</section>
+	</div>
+	`
+	r := strings.NewReader(html)
+	samples, err := parseSamples(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(samples) != 1 || samples[0].in != "10" || samples[0].out != "40" {
+		t.Errorf("unexpected result: %+v", samples)
+	}
+}
