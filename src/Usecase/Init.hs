007cdaffa9ba72f40d1ae18af5c66053659bfd66
fujis <ppdx999@gmail.com>
2025-04-30 21:55:15 +0900

    コメント削除

--- src/Usecase/Init.hs
+++ src/Usecase/Init.hs
@@ -32,14 +32,8 @@ initContest contestId@(ContestId contestName) = do
 
   lift $ logInfo $ "Found " <> T.pack (show $ length problemIds) <> " problems."
 
-  -- 3. Create problem directories using traverse
-  -- traverse はリストの各要素に関数を適用し、結果を収集する (Applicative/Monad の文脈で)
-  -- ここでは、各ディレクトリ作成が ExceptT DomainError m () を返すようにする
   let createProblemDir (ProblemId problemName) = do
         let problemDir = contestDir </> T.unpack problemName
         lift $ logInfo $ "Creating directory: " <> T.pack problemDir
         ExceptT $ createDirectory problemDir -- 失敗したら ExceptT が Left を伝播
 
-  -- traverse createProblemDir problemIds は ExceptT DomainError m [()] 型になる
-  -- いずれか一つでも createProblemDir が Left を返すと、全体の結果が Left になる
-  traverse_ createProblemDir problemIds -- 結果の [()] は不要なので捨てる
