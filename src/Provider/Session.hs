803f04ca860cbf3fb64f9ce66268a905984600fb
fujis <ppdx999@gmail.com>
2025-05-08 13:01:50 +0900

    ファイルが存在しない場合はSessionNotFoundを返すように修正

--- src/Provider/Session.hs
+++ src/Provider/Session.hs
@@ -1,27 +1,32 @@
 module Provider.Session (loadSessionIO, saveSessionIO) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT)
 import qualified Data.Text.Encoding as TEnc
 import Interface (HasFileSystem (..))
 import System.Directory (getHomeDirectory)
-import System.FilePath ((</>))
-import Types (AppError, Session (..), validateSession)
+import System.FilePath (takeDirectory, (</>))
+import Types (AppError (SessionNotFound), Session (..), validateSession)
 import Prelude hiding (readFile)
 
 sessionFilePath :: (MonadIO m) => m FilePath
 sessionFilePath = do
   home <- liftIO getHomeDirectory
-  pure $ home </> ".local" </> "share" </> "atcoder-cil" </> "session.txt"
+  pure $ home </> ".local" </> "share" </> "atcoder-cli" </> "session.txt"
 
 loadSessionIO :: (MonadIO m, HasFileSystem m) => m (Either AppError Session)
-loadSessionIO =
-  sessionFilePath
-    >>= readFile
-    >>= either
-      (return . Left)
-      (return . validateSession . TEnc.decodeUtf8)
+loadSessionIO = do
+  sessionPath <- sessionFilePath
+  isExist <- doesFileExist sessionPath
+  if isExist
+    then
+      readFile sessionPath
+        >>= either (return . Left) (return . validateSession . TEnc.decodeUtf8)
+    else
+      return $ Left SessionNotFound
 
 saveSessionIO :: (MonadIO m, HasFileSystem m) => Session -> m (Either AppError ())
-saveSessionIO (Session session) = do
+saveSessionIO (Session session) = runExceptT $ do
   sessionPath <- sessionFilePath
-  saveFile sessionPath $ TEnc.encodeUtf8 session
+  _ <- ExceptT $ createDirectoryIfMissing True $ takeDirectory sessionPath
+  ExceptT $ saveFile sessionPath $ TEnc.encodeUtf8 session
