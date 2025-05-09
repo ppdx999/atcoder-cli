2420613b5d9a8e2b1f90772921a6ab8c0e32072e
fujis <ppdx999@gmail.com>
2025-05-09 23:48:15 +0900

    testコマンドの実行にログを追加

--- src/Usecase/test.hs
+++ src/Usecase/test.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Usecase.Test (test) where
 
+import Control.Monad.Trans (MonadTrans (lift))
 import Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT)
 import Data.Foldable (traverse_)
+import qualified Data.Text as T
 import Interface (HasLanguage (..), HasLogger (..), HasReporter (..), HasTestCase (..))
 import Types
 
@@ -13,12 +17,28 @@ test ::
   ) =>
   m (Either AppError ())
 test = runExceptT $ do
+  logInfoE "Testing ..."
+
+  logInfoE "Detecting language ..."
   lang <- ExceptT detectLanguage
+  logInfoE $ "Detected language: " <> langName lang
+
+  logInfoE "Building language ..."
   ExceptT $ buildLanguage lang
+
+  logInfoE "Loading test cases ..."
   tcs <- ExceptT loadTestCases
+  logInfoE $ "Found " <> T.pack (show $ length tcs) <> " test cases."
+
+  logInfoE "Running test cases ..."
   traverse_ (ExceptT . runAndReport lang) tcs
+
+  logInfoE "Cleaning up build files ..."
   ExceptT $ cleanupBuildFile lang
   where
+    logInfoE :: (HasLogger m, MonadTrans t) => T.Text -> t m ()
+    logInfoE = lift . logInfo
+
     runAndReport :: (HasLanguage m, HasReporter m) => Language -> TestCase -> m (Either AppError ())
     runAndReport lang tc =
       runTestCase lang tc
